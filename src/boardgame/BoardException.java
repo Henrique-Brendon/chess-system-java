7590afaab4e73ba5ebc4c51d49c7202b26ddc054
Henrique Brendon <henriquebrendonti26@gmail.com>
2023-02-23 23:02:32 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,10 @@
+package boardgame;
+
+public class BoardException extends RuntimeException {
+	public static final long serialVerisonUID = 1L;
+	
+	public BoardException(String msg) {
+		super(msg);
+	}
+
+}
